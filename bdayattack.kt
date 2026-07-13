1e21a80f109cccbb40039d9e9eb7c2524612e984
Aarav R <127631010+AaravR10@users.noreply.github.com>
2026-07-13 13:13:33 -0700

    Update bdayattack.kt

--- bdayattack.kt
+++ bdayattack.kt
@@ -11,28 +11,27 @@ fun hash16(message: String): Int {
 fun main() {
     val baseGoodMessage = "I, Alice, am a pacifist"
     val baseBadMessage = "I, Alice, am an anarchist"
-
     val goodMessageHashes = HashMap<Int, String>()
 
     println("--- Birthday Attack Simulation (Custom 16-bit Hash) ---")
-    println("Target space size: $0xFFFF$ (${0xFFFF + 1} possible values)")
+    println("Target space size: 65536 possible values")
 
-    var attempts = 0
-    while (attempts < 400) {
-        val variant = baseGoodMessage + " ".repeat(Random.nextInt(1, 100))
+    var goodAttempts = 0
+    while (goodMessageHashes.size < 2000) {
+        val variant = "$baseGoodMessage [ID: $goodAttempts]"
         val hash = hash16(variant)
         goodMessageHashes[hash] = variant
-        attempts++
+        goodAttempts++
     }
     println("Stored ${goodMessageHashes.size} unique variations of the GOOD message.")
-
     println("\nSearching for a bad message variation that yields a colliding hash...")
+
     var badAttempts = 0
     var collisionFound = false
 
     while (!collisionFound) {
         badAttempts++
-        val badVariant = baseBadMessage + " ".repeat(Random.nextInt(1, 100))
+        val badVariant = "$baseBadMessage [ID: ${Random.nextInt(1, 1000000)}]"
         val badHash = hash16(badVariant)
 
         if (goodMessageHashes.containsKey(badHash)) {
