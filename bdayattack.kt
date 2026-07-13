113c5723b974ed0202ece1cc6f7aee332f2151f5
Aarav R <127631010+AaravR10@users.noreply.github.com>
2026-07-13 13:50:00 -0700

    Changed to new hash16BitAlpha

--- bdayattack.kt
+++ bdayattack.kt
@@ -1,13 +1,5 @@
 import kotlin.random.Random
 
-fun hash16(message: String): Int {
-    var hash = 5381L
-    for (char in message) {
-        hash = ((hash shl 5) + hash) + char.code
-    }
-    return (hash and 0xFFFF).toInt()
-}
-/*
 fun hash16BitAlpha(message: String): Int{
     var hash = 0
     for (char in message){
@@ -16,7 +8,6 @@ fun hash16BitAlpha(message: String): Int{
     }
     return hash
 }
-*/
 fun main() {
     val baseGoodMessage = "I, Alice, am a pacifist"
     val baseBadMessage = "I, Alice, am an anarchist"
@@ -28,7 +19,7 @@ fun main() {
     var goodAttempts = 0
     while (goodMessageHashes.size < 2000) {
         val variant = "$baseGoodMessage [ID: $goodAttempts]"
-        val hash = hash16(variant)
+        val hash = hash16BitAlpha(variant)
         goodMessageHashes[hash] = variant
         goodAttempts++
     }
@@ -41,7 +32,7 @@ fun main() {
     while (!collisionFound) {
         badAttempts++
         val badVariant = "$baseBadMessage [ID: ${Random.nextInt(1, 1000000)}]"
-        val badHash = hash16(badVariant)
+        val badHash = hash16BitAlpha(badVariant)
 
         if (goodMessageHashes.containsKey(badHash)) {
             val matchingGoodMessage = goodMessageHashes[badHash]
